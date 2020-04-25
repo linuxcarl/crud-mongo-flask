658d85ab60a205ca90736fb14ac39f41db8481d8
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-25 15:20:49 -0500

    se agrego consultar curso por nombre

--- platzi-api/db.py
+++ platzi-api/db.py
@@ -82,5 +82,6 @@ def consultar_curso_por_id_proyeccion(id_curso, proyeccion=None):
 
 
 def consultar_curso_por_nombre(nombre):
-    return str('Falta por implementar')
+    return dumps(db.cursos.find({'$text': {'$search': nombre}}))
+
 
